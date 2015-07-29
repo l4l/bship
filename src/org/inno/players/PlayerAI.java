426f1e9b161735b7ce19be18ce1dfa9b2ed7cf70
Kitsu <mail@kitsu.me>
2015-07-29 10:27:41 +0300

    Added base abstract class PlayerAI

---
+++ src/org/inno/players/PlayerAI.java
@@ -0,0 +1,12 @@
+package org.inno.players;
+
+/**
+ * Created by kitsu.
+ * This file is part of BattleShip in package org.inno.players.
+ */
+public abstract class PlayerAI extends Player {
+
+    public PlayerAI() {
+        init(this, false);
+    }
+}
--- src/org/inno/players/PlayerAIStupid.java
+++ src/org/inno/players/PlayerAIStupid.java
@@ -1,9 +1,6 @@
 package org.inno.players;
 
-import org.inno.game.Cell;
-import org.inno.game.Coord;
-import org.inno.game.Ship;
-import org.inno.game.ShipType;
+import org.inno.game.*;
 
 import java.util.Random;
 
@@ -11,18 +8,14 @@ import java.util.Random;
  * Created by kitsu.
  * This file is part of BattleShip in package org.inno.
  */
-public class PlayerAIStupid extends Player {
-
-    public PlayerAIStupid() {
-        init(this);
-    }
+public class PlayerAIStupid extends PlayerAI {
 
     @Override
     public Coord move() {
         Coord c;
         Random r = new Random();
         do {
-            c = new Coord(r.nextInt() % SEA_SIZE, r.nextInt() % SEA_SIZE);
+            c = new Coord(r.nextInt() % Sea.SEA_DEFAULT_SIZE, r.nextInt() % Sea.SEA_DEFAULT_SIZE);
         } while (enemy.getCell(c.x, c.y) != Cell.EMPTY);
         return c;
     }
@@ -32,7 +25,7 @@ public class PlayerAIStupid extends Player {
         Ship p;
         Random r = new Random();
         do {
-            p = new Ship(ship, r.nextInt() % SEA_SIZE, r.nextInt() % SEA_SIZE, r.nextBoolean());
+            p = new Ship(ship, r.nextInt() % Sea.SEA_DEFAULT_SIZE, r.nextInt() % Sea.SEA_DEFAULT_SIZE, r.nextBoolean());
         } while (self.placeable(p));
         return p;
     }
